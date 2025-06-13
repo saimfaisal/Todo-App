dfaf73c1627a917eb7ca25b843428675a8967499
saim <saimfaisal.official.com@gmail.com>
2025-06-13 05:27:31 -0700

    change function

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -46,8 +46,8 @@ function App() {
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-      <div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto bg-[#203f83] shadow-md rounded-lg px-4 py-3 text-white">
+      <div className="bg-[#a25eda] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto bg-[#745cb4] shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
